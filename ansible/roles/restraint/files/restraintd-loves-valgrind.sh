4805fa3b8b6cdcccf1f507e8f3ce2c1ae2fbdb82
Daniel Rodríguez <danrodri@redhat.com>
2020-06-06 00:06:59 +0200

    feat: Move valgrind log to /var/tmp
    Log in /var/tmp should persist across reboots

--- ansible/roles/restraint/files/restraintd-loves-valgrind.sh
+++ ansible/roles/restraint/files/restraintd-loves-valgrind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VALGRIND_LOG=${VALGRIND_LOG:-/tmp/restraintd-vg_$(date +%s).log}
+VALGRIND_LOG=${VALGRIND_LOG:-/var/tmp/restraintd-vg_$(date +%s).log}
 
 exec valgrind --leak-check=full --log-file="${VALGRIND_LOG}" restraintd "$@"
