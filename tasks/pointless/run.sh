c59fb56de10a7d0517b81a6a88f8d9949e2c82a9
Daniel Rodríguez <danrodri@redhat.com>
2020-06-11 11:15:26 +0200

    feat: Add ability to define sleep time for pointless task

--- tasks/pointless/run.sh
+++ tasks/pointless/run.sh
@@ -2,7 +2,8 @@
 
 echo "I'm a pointless task"
 echo "and I'm going to sleep now..."
+echo "for ${POINTLESS_SLEEP_TIME:=5} seconds..." >&2
 
-sleep 5
+sleep "${POINTLESS_SLEEP_TIME}"
 
 echo "I'm done..."
